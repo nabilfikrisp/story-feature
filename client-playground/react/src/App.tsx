f9898e38fa02a436abd7a131e7011a586fe36668
nabilfikrisp <mnabilfikrisp@gmail.com>
2025-12-29 14:53:27 +0700

    initialize react vite tailwind

---
+++ client-playground/react/src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <div
+        id="container"
+        className="mx-auto h-full w-full max-w-4xl flex-1 px-5 py-10"
+      >
+        Hello
+      </div>
+    </div>
+  );
+}
